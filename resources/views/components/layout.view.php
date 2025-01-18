43d6f0a66413905edd3dd7b15149d9c18236ee41
developedbybarley@gmail.com <71340344+DevelopedByBarley@users.noreply.github.com>
2025-01-18 18:09:40 +0100

    basic scss

--- resources/views/components/layout.view.php
+++ resources/views/components/layout.view.php
@@ -5,11 +5,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="/public/node_modules/bootstrap/dist/css/bootstrap.min.css?v=<?= time() ?>">
+  <link rel="stylesheet" href="/resources/css/main.css">
   <title><?= APP_NAME ?></title>
 </head>
 
 <body data-bs-theme="dark">
-
+  
   <?php require_once view_path('components/navbar') ?>
 
   <?= $root ?>
--- resources/views/welcome.view.php
+++ resources/views/welcome.view.php
@@ -1,27 +1,11 @@
-<div class="mt-5 container-fluid p-5 bg-primary text-white text-center">
-  <h1>MY FIRST Page</h1>
-  <p>Resize this responsive page to see the effect!</p>
-</div>
 
-<?= $id ?? '' ?>
 
-<div class="container mt-5">
+<div class="contianer pr-font">
   <div class="row">
-    <div class="col-sm-4">
-      <h3>Column 1</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-      
-    </div>
-    <div class="col-sm-4">
-      <h3>Column 2</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-    <div class="col-sm-4">
-      <h3>Column 3</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    <div class="col-12 vh-100 d-flex align-items-center justify-content-center">
+      <div>
+        <h1>Welcome</h1>
+      </div>
     </div>
   </div>
 </div>
