c9b00715f79665c44e3f13498022f5f212d9ce6c
developedbybarley@gmail.com <71340344+DevelopedByBarley@users.noreply.github.com>
2025-01-20 17:56:20 +0100

    All layout js and css import upgrade

--- resources/views/components/admin-layout.view.php
+++ resources/views/components/admin-layout.view.php
@@ -4,7 +4,9 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/public/node_modules/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/resources/bootstrap/css/bootstrap.css?v=<?= time() ?>">
+  <link rel="stylesheet" href="/resources/css/main.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
   <title><?= APP_NAME ?></title>
 </head>
 
@@ -16,7 +18,7 @@
   <!--Footer-->
   
   <script type="module" src="/resources/js/main.js"></script>
-  <script src="/public/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+  <script src="resources/bootstrap/js/bootstrap.bundle.js?v=<?= time() ?>"></script>
 </body>
 
 </html>
--- resources/views/status/419.view.php
+++ resources/views/status/419.view.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css?v=<?= time() ?>">
+    <link rel="stylesheet" href="/resources/bootstrap/css/bootstrap.css?v=<?= time() ?>">
     <title><?= APP_NAME ?> - Session Expired</title>
 </head>
 
